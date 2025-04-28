ddb4b9671e3c4a7813d0c20242718852c937cf20
Sefinek <contact@sefinek.net>
2025-04-28 15:30:44 +0200

    Update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const helmet = require('helmet');
-// const axios = require('axios');
-// const fs = require('node:fs');
-// const path = require('node:path');
+const axios = require('axios');
+const fs = require('node:fs');
+const path = require('node:path');
 const morgan = require('./middlewares/morgan.js');
 const rateLimit = require('./middlewares/ratelimit.js');
 const timeout = require('./middlewares/timeout.js');
@@ -21,34 +21,34 @@ app.use(rateLimit);
 app.use(timeout());
 app.use(token);
 
-// app.get('/api/v1/nsfw-classification', async (req, res) => {
-// 	const { path: imagePath, url: imageUrl } = req.query;
-//
-// 	try {
-// 		if (imagePath) {
-// 			const resolvedPath = path.resolve(imagePath);
-// 			if (!fs.existsSync(resolvedPath) || fs.lstatSync(resolvedPath).isDirectory()) return res.status(400).send('Invalid file path.');
-// 			const imageBuffer = await fs.promises.readFile(resolvedPath);
-//
-// 			console.log(`Classifying image from path: ${imagePath}...`);
-// 			const result = await nsfwService.classifyImage(imageBuffer);
-// 			return res.json(result);
-// 		}
-//
-// 		if (imageUrl) {
-// 			const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-//
-// 			console.log(`Classifying image from URL: ${imageUrl}...`);
-// 			const result = await nsfwService.classifyImage(response.data);
-// 			return res.json(result);
-// 		}
-//
-// 		res.status(400).json({ success: false, status: 400, message: 'No image path or URL provided.' });
-// 	} catch (err) {
-// 		console.error(err);
-// 		res.status(500).json({ success: false, status: 500 });
-// 	}
-// });
+app.get('/api/v1/nsfw-classification', async (req, res) => {
+	const { path: imagePath } = req.query;
+
+	try {
+		if (imagePath) {
+			const resolvedPath = path.resolve(imagePath);
+			if (!fs.existsSync(resolvedPath) || fs.lstatSync(resolvedPath).isDirectory()) return res.status(400).send('Invalid file path.');
+			const imageBuffer = await fs.promises.readFile(resolvedPath);
+
+			console.log(`Classifying image from path: ${imagePath}...`);
+			const result = await nsfwService.classifyImage(imageBuffer);
+			return res.json(result);
+		}
+
+		// if (imageUrl) {
+		// 	const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+		//
+		// 	console.log(`Classifying image from URL: ${imageUrl}...`);
+		// 	const result = await nsfwService.classifyImage(response.data);
+		// 	return res.json(result);
+		// }
+
+		res.status(400).json({ success: false, status: 400, message: 'No image path.' });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ success: false, status: 500 });
+	}
+});
 
 app.use((req, res) => res.status(404).json({ success: false, status: 404 }));
 app.use((err, req, res, next) => {
